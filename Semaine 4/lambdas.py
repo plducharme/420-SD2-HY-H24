c2474821df499ce6fcf7c4a71f6b5baf23bed4ef
Pier Luc Ducharme <dutch80@gmail.com>
2024-02-15 15:44:17 -0500

    modif typo

--- Semaine 4/lambdas.py
+++ Semaine 4/lambdas.py
@@ -2,7 +2,7 @@
 
 # Déclarer un lambda à plusieurs variables
 
-# Déclarer un lambda dont les variables seront utilisés par le constructeur et utiliser le lambda
+# Déclarer un lambda dont les variables seront utilisés par le constructeur d'une classe et utiliser le lambda
 
 
 
