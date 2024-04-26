e58b61fb41dbed6340ad0a554502c768a7600186
Pier Luc Ducharme <dutch80@gmail.com>
2024-04-26 16:08:40 -0400

    correction semaine 12

--- Semaine 12/exemples/pickle_exemple.py
+++ Semaine 12/exemples/pickle_exemple.py
@@ -2,16 +2,16 @@ import pickle
 
 dictionnaire = {'The Super Mario Bros. Movie': 30396285, 'John Wick: Chapter 4': 2460347, 'Air': 2322850}
 
-# Serialisation
+# Sérialisation
 with open('dict.pickle', 'wb') as pickle_file:
     pickle.dump(dictionnaire, pickle_file)
 print('Objet sauvegarde: ' + str(dictionnaire))
 
-# Deserialisation
+# Désérialisation
 dictionnaire_charge = None
 with open('dict.pickle', 'rb') as pickle_in:
     dictionnaire_charge = pickle.load(pickle_in)
-print('Objet charge: ' + str(dictionnaire_charge))
+print('Objet chargé: ' + str(dictionnaire_charge))
 
 if dictionnaire == dictionnaire_charge:
-    print('Equivalent')
+    print('Équivalent')
